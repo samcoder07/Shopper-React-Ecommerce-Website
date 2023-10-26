505959f55ae289e540ebe4f8eb1ef46002683a8e
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2023-10-26 22:53:36 +0530

    products section created

--- shopper-ecommerce-website/src/App.js
+++ shopper-ecommerce-website/src/App.js
@@ -7,7 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div>
@@ -15,9 +17,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category='mens'/>}/>
-        <Route path='/womens' element={<ShopCategory ategory='womens'/>}/>
-        <Route path='/kids' element={<ShopCategory ategory='kids'/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category='mens'/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} ategory='womens'/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} ategory='kids'/>}/>
         <Route path='/product'element={<Product/>} >
         <Route path=':productId' element={<Product/>}/>
         </Route>
