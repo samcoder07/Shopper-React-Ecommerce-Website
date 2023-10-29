6edede4936e5aac31e4cadaec0f9c8374845272a
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2023-10-29 22:49:02 +0530

    added product description review

--- shopper-ecommerce-website/src/Components/DescriptionBox/DescriptionBox.jsx
+++ shopper-ecommerce-website/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './DescriptionBox.css'
 const DescriptionBox = () => {
-  return (
-    <div className='descriptionbox'>
-      
-    </div>
-  )
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">
+                    Description
+                </div>
+                <div className="descriptionbox-nav-box fade">
+                    Reviews (122)
+                </div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam repellat veritatis fuga, cum quaerat inventore, autem architecto cupiditate ullam, libero nam voluptas voluptatibus delectus! Alias asperiores quam error cumque aut.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste deserunt molestias, quo aperiam reiciendis facere ratione autem animi. Animi, soluta?</p>
+            </div>
+        </div>
+    )
 }
 
 export default DescriptionBox
