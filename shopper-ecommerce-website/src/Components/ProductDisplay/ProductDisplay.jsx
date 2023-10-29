3eb94939d8d2b1c0e8268074226907d6420cf3e6
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2023-10-29 12:29:38 +0530

    checkin

--- shopper-ecommerce-website/src/Components/ProductDisplay/ProductDisplay.jsx
+++ shopper-ecommerce-website/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import './ProductDisplay.css'
 const ProductDisplay = (props) => {
     const product = props;
-    console.log(product.image);
+
+    console.log(product);
   return (
     <div className='productDisplay'>
       <div className="productdisplay-left">
        <div className="productdisplay-img-list">
-        <img src={product.image} alt="" />
-        <img src={product.image} alt="" />
-        <img src={product.image} alt="" />
-        <img src={product.image} alt="" />
+        <img src={product.product.image} alt="" />
+        <img src={product.product.image} alt="" />
+        <img src={product.product.image} alt="" />
+        <img src={product.product.image} alt="" />
        </div>
        <div className="productdisplay-img">
-        <img className='productdisplay-main-img' src={product.image} alt="" />
+        <img className='productdisplay-main-img' 
+        src={product.image} alt="" />
        </div>
       </div>
       <div className="productdisplay-right">
