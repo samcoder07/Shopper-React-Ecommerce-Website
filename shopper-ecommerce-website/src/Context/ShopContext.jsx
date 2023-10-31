1c5887d33ac623cff181c8df1c85f9a9360555d6
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2023-10-31 23:20:30 +0530

    checkin

--- shopper-ecommerce-website/src/Context/ShopContext.jsx
+++ shopper-ecommerce-website/src/Context/ShopContext.jsx
@@ -1,15 +1,36 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
 export const ShopContext = createContext([]);
+    
+const getDefaultCart = () =>{
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++) {
+       cart[index]=0;
+        
+    }
+    return cart;
+    
+}
 
 const ShopContextProvider = (props) =>{
-    const contextValue = {all_product};
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+    
+    const addToCart = (itemId) =>{
+      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+      console.log(cartItems);
+    }
 
+    const removeFromCart = (itemId) =>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+      }
+
+      const contextValue = {all_product, cartItems,addToCart,removeFromCart};
     useEffect(() => {
 
     }, [contextValue.length])
-    
+
+
     return(
         <ShopContext.Provider value={
             {
